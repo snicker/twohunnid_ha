e5a824cd68601c30b5b7feb94a67b61a3f4013e5
Nick Gordon <ngordon779@gmail.com>
2020-04-11 21:37:29 -0700

    support bright and dim commands for x10rf

--- custom_components/x10rf/light.py
+++ custom_components/x10rf/light.py
@@ -15,6 +15,7 @@ from homeassistant.components.light import (
     ATTR_BRIGHTNESS, PLATFORM_SCHEMA)
 from homeassistant.components.x10.light import X10Light, x10_command
 import homeassistant.helpers.config_validation as cv
+import math
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -41,3 +42,28 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
 class X10RFLight(X10Light):
     """Representation of an X10 RF Light."""
+
+    def __init__(self, light, is_cm11a):
+        """Initialize an X10rf Light."""
+        X10Light.__init__(self, light, is_cm11a)
+        self._brightness_step = 0
+
+
+    def turn_on(self, **kwargs):
+        """Instruct the light to turn on."""
+        if self._is_cm11a:
+            x10_command(f"on {self._id}")
+        else:
+            x10_command(f"fon {self._id}")
+        brightness = kwargs.get(ATTR_BRIGHTNESS)
+        if brightness is not None and brightness > 0:
+            cur_step = math.ceil(self._brightness / 4.0)
+            new_step = math.ceil(brightness / 4.0))
+            delta = new_step - cur_step
+            if delta != 0:
+                command = "bright" if delta > 0 else "dim"
+                rf = "" if self._is_cm11a else "f"
+                step = abs(delta)
+                x10_command(f"{rf}{command} {self._id} {step}")
+                self._brightness = math.ceil(brightness /4.0) - 1
+        self._state = True
